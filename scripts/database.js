a601b0030e067f7f074ebd7990c48966e5275ff3
KippMinton <luciankippminton@gmail.com>
2021-05-27 14:37:58 -0500

    added fish to list

--- scripts/database.js
+++ scripts/database.js
@@ -2,12 +2,52 @@ export const database = {
     fish: [
         {   
             image: "./images/bartfish.gif",
-            name: "Bart",
-            species: "fish",
-            length: 3,
-            location: "Springfield",
-            food: "crustaceans"
+            name: "Name: Bart",
+            species: "Species: fish",
+            length: "Length: 3",
+            location: "Location: Springfield",
+            food: "Diet: crustaceans"
+        },
+        {   
+            image: "./images/giantCarp.jpeg",
+            name: "Name: Siezer",
+            species: "Species: Carpe dayummm",
+            length: "Length: bigger than regular carp",
+            location: "Location: In the water near damns",
+            food: "Diet: Daylight/photosynthesis"
+        },
+        {   
+            image: "./images/starfish.jpeg",
+            name: "Name: Sirius",
+            species: "Species: Piscis astronomus",
+            length: "Length: 1 - 2 lightyears",
+            location: "Location: the night sky",
+            food: "Diet: your wishes"
+        },
+        {   
+            image: "./images/redfish.jpeg",
+            name: "Name: Ol' Red",
+            species: "Species: Pescatarius rufus",
+            length: "Length: 12 feet",
+            location: "Location: Red River",
+            food: "Diet: rednecks"
+        },
+        {   
+            image: "./images/tomfish.jpeg",
+            name: "Name: Tom Waits",
+            species: "Species: Swordfishicus trombonicus",
+            length: "Length: Depends on the note",
+            location: "Location: Underground",
+            food: "Diet: Rainbirds"
+        },
+        {
+           image: "./images/cuttlefish.jpeg",
+           name: "Name: Cuddles",
+           species: "Species: Sepia officinalis",
+           length: "Length: 45cm",
+           location: "Location: Mediterranean Sea",
+           food: "Diet: crustaceans, molluscs, small fish"
         }
-    ]
+      ]
 }
 
